ee2d0dce9eb3c7f3625634aa29ff387f5ddbdf7b
Lrakotoson <rakotosonloicandoniaina@gmail.com>
2020-04-21 18:16:55 +0200

    BP | Stack barplot

--- S2/Data Visualization/Budget Pariticipatif/scripts/plotbar.py
+++ S2/Data Visualization/Budget Pariticipatif/scripts/plotbar.py
@@ -10,7 +10,7 @@ def barPlot(pj_source):
     :return: bokeh figure
     """
     if len(pj_source.data) <= 1: # Filtre restreint
-        plot = figure(title = "Projets par quartiers (VIDE)")
+        plot = figure(title = "Projets par quartiers (VIDE)", tools = "")
         plot.vbar(0, 0)
         return plot
 
@@ -44,6 +44,65 @@ def barPlot(pj_source):
     plot.xaxis.major_tick_line_width = 0
     plot.xaxis.axis_line_width = 0
     plot.xaxis.group_label_orientation = np.pi/2
+    plot.legend.orientation = "horizontal"
+    plot.legend.background_fill_alpha = 0.5
+    
+    return plot
+
+
+def stackPlot(et_source):
+    """
+    Stack barplot de l'état des projets par année
+    et_source: etat ColumnDataSource
+    :return: bokeh figure
+    """
+    
+    if len(et_source.data) <= 1: # Filtre restreint
+        plot = figure(title = "Etat des projets par année (VIDE)", tools = "")
+        plot.vbar(0, 0)
+        return plot
+    
+    colormap = {
+        'Realise': '#2ecc71',
+        'Non_realisable': '#c0392b',
+        "Etude": '#f0932b',
+        'En_cours': '#f1c40f'
+    }
+    
+    etats = [ # Pour l'ésthétique, dans cet ordre
+        etat for etat in ['Realise', "Etude", 'En_cours', 'Non_realisable']
+        if etat in et_source.data.keys()
+    ]
+
+    colors = [colormap[etat] for etat in etats]
+
+    plot = figure(
+        x_range = [],
+        title = "Etat des projets par année",
+        tools = ""
+    )
+
+    renderers = plot.vbar_stack(
+        etats, x = 'b_year', color = colors,
+        source = et_source, legend_label = etats,
+        width = 0.9, name = etats
+    )
+
+    for r in renderers:
+        etat = r.name
+        hover = HoverTool(
+            tooltips = [
+                ("%s total" % etat, "@%s" % etat),
+                ("Année", "@b_year")
+            ],
+            renderers = [r]
+        )
+        plot.add_tools(hover)
+
+
+    plot.x_range.factors = list(et_source.data['b_year'])
+    plot.toolbar.logo = None
+    plot.xgrid.grid_line_color = None
     plot.legend.orientation = "horizontal"
     plot.legend.background_fill_alpha = 0.5
     
